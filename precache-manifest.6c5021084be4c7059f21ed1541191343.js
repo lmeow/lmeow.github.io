8d07295ef6d99a7a215ea48b723d8e681d84023e
Maja Frankiewicz <majafrank@live.se>
2020-08-26 22:41:18 +0200

    Updates

---
+++ precache-manifest.6c5021084be4c7059f21ed1541191343.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "155a63c14772a6ab4cc0aa4b1d089a74",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25cbb75be81a2edc2fb7",
+    "url": "/static/css/main.e0b75249.chunk.css"
+  },
+  {
+    "revision": "ab3f21c8e2b9647a5a66",
+    "url": "/static/js/2.a63c84a1.chunk.js"
+  },
+  {
+    "revision": "25cbb75be81a2edc2fb7",
+    "url": "/static/js/main.88681e41.chunk.js"
+  },
+  {
+    "revision": "71655dba884a582eb297",
+    "url": "/static/js/runtime~main.5c9400fd.js"
+  },
+  {
+    "revision": "51814bcb00b83c033bc1a7ce19b8f8be",
+    "url": "/static/media/plants.51814bcb.svg"
+  }
+]);
