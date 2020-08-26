31159b9beb150c6c8ee23863d461dc6dab02b893
Maja Frankiewicz <majafrank@live.se>
2020-08-26 22:50:28 +0200

    Updates

---
+++ precache-manifest.7d9dcbb9858e2dd83381309818e41746.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "153eee5b1f430469f97c94627b5204b5",
+    "url": "/lmeow.github.io/index.html"
+  },
+  {
+    "revision": "7d7ea79d5626faa8fcec",
+    "url": "/lmeow.github.io/static/css/main.7d400111.chunk.css"
+  },
+  {
+    "revision": "8ec9d54e296c956bbd6c",
+    "url": "/lmeow.github.io/static/js/2.5a39d001.chunk.js"
+  },
+  {
+    "revision": "7d7ea79d5626faa8fcec",
+    "url": "/lmeow.github.io/static/js/main.a1f54061.chunk.js"
+  },
+  {
+    "revision": "a017de387b21cea04e84",
+    "url": "/lmeow.github.io/static/js/runtime~main.a2751cc9.js"
+  },
+  {
+    "revision": "51814bcb00b83c033bc1a7ce19b8f8be",
+    "url": "/lmeow.github.io/static/media/plants.51814bcb.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b5880e35cfdf3894f8552d0dceba6e64.js"
+  "/lmeow.github.io/precache-manifest.7d9dcbb9858e2dd83381309818e41746.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/lmeow.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
